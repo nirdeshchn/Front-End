e5d6c087ecc0b8733c21ad14800dcb10c946dba4
nirdeshchn <nirdeshchn7@gmail.com>
2023-02-24 09:20:53 +0545

    Learning Java Script started !

---
+++ Javascrip/Practice/Namaste world.js
@@ -0,0 +1,12 @@
+let namasteBtn=document.querySelector('button');
+
+namasteBtn.addEventListener('click',inputMsg);
+
+
+function inputMsg(){
+    let name=prompt('Enter Name of Student');
+    namasteBtn.textContent= 'Roll No. 1:'+name;
+    
+}
+
+
